1cacce90751c5a7d03bfcfe8dc7f475fa5411496
TPPSellinger <52659715+TPPSellinger@users.noreply.github.com>
2019-09-12 09:26:34 +0200

    Update install_eSMC.R

--- install_eSMC.R
+++ install_eSMC.R
@@ -3,5 +3,5 @@
 ##########
 install.packages("devtools")
 library(devtools)
-path="~Downloads/eSMC_0.1.0.tar.gz" # Path to the dowloaded eSMC package
+path="~Downloads/eSMC_0.2.0.tar.gz" # Path to the dowloaded eSMC package
 devtools::install_local(path)
