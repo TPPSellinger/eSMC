0e702bfb4f76a863b0023b0c1f7ad9b66d7ccac2
TPPSellinger <52659715+TPPSellinger@users.noreply.github.com>
2020-09-24 09:57:23 +0200

    Update install_eSMC.R

--- install_eSMC.R
+++ install_eSMC.R
@@ -5,3 +5,6 @@ install.packages("devtools")
 library(devtools)
 path="~Downloads/eSMC_1.0.5.tar.gz" # Path to the dowloaded eSMC package
 devtools::install_local(path)
+#to install without root permissions (in a local folder) use this command:
+withr::with_libpaths(new = "~/R/your_local_dir", install_local("eSMC_1.0.5.tar.gz"))
+
